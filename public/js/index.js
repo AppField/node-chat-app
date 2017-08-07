03b626db224a920f470de08ef4e0b5debaa09df9
WebField <roman@muehlfeldner.net>
2017-08-07 18:56:17 +0200

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -8,10 +8,29 @@ socket.on('disconnect', function () {
     console.log('Disconnected from server');
 });
 
-socket.on('newEmail', function (email) {
-    console.log('New Email', email);
-});
-
 socket.on('newMessage', function (message) {
     console.log('newMessage', message);
+    let li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+    $('#messages').append(li);
+});
+
+// socket.emit('createMessage', {
+//     from: 'Frank',
+//     text: 'Hi'
+// }, function (data) {
+//     console.log(data);
+// });
+
+$('#message-form').on('submit', function (event) {
+    event.preventDefault();
+    // let from = $("[name='from']").val();
+    let text = $("[name='text']").val();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: text
+    }, function (data) {
+        console.log('message send');
+    });
 });
